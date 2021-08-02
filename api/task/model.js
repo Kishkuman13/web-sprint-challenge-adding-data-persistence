aa5c57448171a54aa793470fc6fe8e403ccbacb7
Kishkuman13 <73566523+Kishkuman13@users.noreply.github.com>
2021-08-01 19:04:27 -0600

    fixed errors

--- api/task/model.js
+++ api/task/model.js
@@ -25,23 +25,31 @@ async function findAll() {
 }
 
 async function addTask(taskData) {
-  const [ task_id ] = await db('tasks')
+  const [ 
+    task_id, 
+    task_description, 
+    task_notes, 
+    task_completed, 
+    project_id, 
+    project_name, 
+    project_description 
+  ] = await db('tasks')
     .join('projects', 'projects_id', 'tasks.project_id')
     .select('tasks.*', 'projects.id')
     .insert(taskData);
 
   const tasksObj = {
     task_id: task_id,
-    task_description: task.task_description,
-    task_notes: !task.task_notes ? null : task.task_notes,
-    task_completed: !task.task_completed
+    task_description: task_description,
+    task_notes: !task_notes ? null : task_notes,
+    task_completed: !task_completed
       ? false
-      : task.task_completed === 0
+      : task_completed === 0
       ? false
       : true,
-    project_id: task.project_id,
-    project_name: task.project_name,
-    project_description: task.project_description
+    project_id: project_id,
+    project_name: project_name,
+    project_description: project_description
   };
   return tasksObj;
 }
